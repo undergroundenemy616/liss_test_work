feed2a625968691021e63ba999ebca88ba63917f
ptrofimov <ptrofimov@emphasoft.com>
2021-03-14 16:50:35 +0300

    small fix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = os.environ.get('8x+^)*br(0!nkc&r^4t4o1d^zo((#+h(f_q87c93$0!^b!71^e')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
@@ -11,24 +11,11 @@ class Config:
         pass
 
 
-#example how to develop different database configuration for different stage
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///DataBase.db'
 
 
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////DataBase.db'
-
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////DataBase.db'
-
-
 config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig,
     'default': DevelopmentConfig
 }
